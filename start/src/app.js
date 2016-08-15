a90701835bfa8f7d4960c4bbb6d54081cfa64ac6
Dayne Davis <daynedavis5@gmail.com>
2016-08-15 19:14:15 -0400

    added filtered list example

--- start/src/app.js
+++ start/src/app.js
@@ -18,7 +18,56 @@ var MyComponent = React.createClass({
         );
     }
 });
+
+var FilteredList = React.createClass({
+    filterList: function(event) {
+        var updatedList = this.state.initialItems;
+        updatedList = updatedList.filter(function(item) {
+            return item.toLowerCase().search(
+                event.target.value.toLowerCase()) !== -1;
+        });
+        this.setState({items: updatedList});
+    },
+    getInitialState: function() {
+        return {
+            initialItems: [
+                'Apples',
+                'Brocolli',
+                'Chicken',
+                'Duck',
+                'Eggs'
+            ],
+            items: []
+        }
+    },
+    componentWillMount: function() {
+        this.setState({items: this.state.initialItems})
+    },
+    render: function() {
+        return (
+            <div className="filter-list">
+                <input type="text" placeholder="Search" onChange={this.filterList}/>
+            <List items={this.state.items}/>
+            </div>
+        );
+    }
+});
+
+var List = React.createClass({
+    render: function() {
+        return (
+            <ul>
+            {
+                this.props.items.map(function(item) {
+                    return <li key={item}>{item}</li>
+                })
+            }
+            </ul>
+        )
+    }
+});
+
 ReactDOM.render(
-    <MyComponent/>,
+    <FilteredList/>,
     document.getElementById('mount-point')
 );
